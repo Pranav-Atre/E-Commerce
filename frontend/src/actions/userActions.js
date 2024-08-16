e938992906db47acd613a717e88b3c05f83dca0f
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-08-16 13:09:33 +0530

    Update

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -78,7 +78,7 @@ export const register = (userData) => async (dispatch) => {
 export const loadUser = () => async (dispatch) => {
     try {
         dispatch({ type: LOAD_USER_REQUEST });
-        const { data } = await axios.get(`/api/v1/me`);
+        const { data } = await axios.get(`https://e-commerce-zrqz.onrender.com/api/v1/me`);
         dispatch({
             type: LOAD_USER_SUCCESS,
             payload: data
