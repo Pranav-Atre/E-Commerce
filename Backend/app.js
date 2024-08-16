20bb99e3b1a1df5190f8ce83e92948c957631876
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-08-16 17:23:02 +0530

    Made Changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+
 dotenv.config(); 
 
+// Set up CORS to allow requests from your frontend (Vercel)
+const allowedOrigins = [process.env.FRONTEND_URL];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // Enable sending cookies
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,10 +36,9 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-module.exports =  app;
+module.exports = app;
