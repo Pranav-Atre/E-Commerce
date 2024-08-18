e939ca907277eddade11a5b5daece4d7e411e20a
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-08-19 00:09:27 +0530

    Updated

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -83,7 +83,10 @@ export const register = (userData) => async (dispatch) => {
 export const loadUser = () => async (dispatch) => {
     try {
         dispatch({ type: LOAD_USER_REQUEST });
-        const { data } = await axios.get(`${API_BASE_URL}/api/v1/me`);
+        const { data } = await axios.get(`${API_BASE_URL}/api/v1/me`, {
+            withCredentials: true,  // Include cookies in the request
+          }
+        );
         dispatch({
             type: LOAD_USER_SUCCESS,
             payload: data
